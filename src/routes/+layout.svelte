8a2f2aaa8a6c68039d8881efcc9dafacf505fcb7
Benjamin <57356716+t7ru@users.noreply.github.com>
2026-02-17 08:27:56 -0500

    feat: support for apple home icon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,15 @@
 <script>
 	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+	<title>[TEST] TDS Statistics Editor</title>
+	<link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+	<link rel="icon" href="/favicon.ico" />
+	<link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous">
 	<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
