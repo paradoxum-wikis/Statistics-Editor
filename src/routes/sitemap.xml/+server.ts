8c4fb4cab5ec4f2b8b152733839baf6fabd55b00
Benjamin <7@toru.ca>
2026-07-15 01:46:42 -0400

    feat: sitemap

---
+++ src/routes/sitemap.xml/+server.ts
@@ -0,0 +1,22 @@
+import { towerNames } from "$lib/towerComponents/towers";
+
+export const prerender = true;
+
+export function GET() {
+  const urls = [
+    `	<url><loc>https://se.tds.wiki/</loc></url>`,
+    ...towerNames.map((name) => {
+      const url = `https://se.tds.wiki/tower/${encodeURIComponent(name)}`;
+      return `	<url><loc>${url}</loc></url>`;
+    }),
+  ].join("\n");
+
+  const body = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${urls}
+</urlset>`;
+
+  return new Response(body, {
+    headers: { "Content-Type": "application/xml" },
+  });
+}
